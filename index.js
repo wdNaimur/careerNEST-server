f97184885ab9a6860bfdd271cf8016a2375865a9
NaimurWD <naimur.wd@gmail.com>
2025-06-01 16:23:26 +0600

    Specific user's application and delete application api added

--- index.js
+++ index.js
@@ -26,20 +26,55 @@ async function run() {
     await client.connect();
 
     const jobsCollection = client.db("careerNest").collection("jobs");
+    const applicationsCollection = client
+      .db("careerNest")
+      .collection("applications");
 
     // start jobs API
+    //Get All Jobs
     app.get("/jobs", async (req, res) => {
       const cursor = jobsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
+    // Get single Job Details
     app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await jobsCollection.findOne(query);
       res.send(result);
     });
-
+    // Job Applications Related Api
+    app.get("/applications", async (req, res) => {
+      const email = req.query.email;
+      const query = { userEmail: email };
+      const result = await applicationsCollection.find(query).toArray();
+      for (const application of result) {
+        const jobId = application.jobId;
+        const jobQuery = { _id: new ObjectId(jobId) };
+        const jobDetails = await jobsCollection.findOne(jobQuery);
+        application.company_logo = jobDetails.company_logo;
+        application.company = jobDetails.company;
+        application.title = jobDetails.title;
+        application.applicationDeadline = jobDetails.applicationDeadline;
+        application.description = jobDetails.description;
+        const deadline = new Date(jobDetails.applicationDeadline);
+        const today = new Date();
+        application.status = today <= deadline;
+      }
+      res.send(result);
+    });
+    app.delete("/applications/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await applicationsCollection.deleteOne(query);
+      res.send(result);
+    });
+    app.post("/applications", async (req, res) => {
+      const application = req.body;
+      const result = await applicationsCollection.insertOne(application);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
